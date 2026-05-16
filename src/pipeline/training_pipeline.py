ec4ac82885240441f8861f20f508fd70d875d771
Shreyaan <shreyaan.l23@iiits.in>
2026-05-16 10:37:52 +0530

    training and inference pipeline

---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_preprocessing import DataPreprocessing
+from src.components.recommender import (
+    ContentBasedRecommender,
+    UserBasedRecommender,
+    HybridRecommender,
+)
+from src.constants import SPLIT_FILE_NAME, RATINGS_SPLIT_FILE_NAME, ANIME_FILE_NAME
+from src.entity.config_entity import (
+    DataIngestionConfig,
+    DataPreprocessingConfig,
+    ContentBasedRecommenderConfig,
+    UserBasedRecommenderConfig,
+    HybridRecommenderConfig,
+)
+
+
+class TrainingPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.data_preprocessing_config = DataPreprocessingConfig()
+        self.content_based_config = ContentBasedRecommenderConfig()
+        self.user_based_config = UserBasedRecommenderConfig()
+        self.hybrid_config = HybridRecommenderConfig()
+
+    def run(self):
+        data_ingestion = DataIngestion(self.data_ingestion_config)
+        data_ingestion.run()
+
+        data_preprocessing = DataPreprocessing(self.data_preprocessing_config)
+        data_preprocessing.run()
+
+        train_df = pd.read_csv(os.path.join(self.data_preprocessing_config.train_data_dir, SPLIT_FILE_NAME))
+        content_recommender = ContentBasedRecommender(self.content_based_config)
+        content_artifact = content_recommender.run(train_df)
+
+        ratings_df = pd.read_csv(os.path.join(self.data_preprocessing_config.train_data_dir, RATINGS_SPLIT_FILE_NAME))
+        user_recommender = UserBasedRecommender(self.user_based_config)
+        user_artifact = user_recommender.run(ratings_df, content_artifact)
+
+        anime_df = pd.read_csv(
+            os.path.join(self.data_preprocessing_config.raw_data_dir, ANIME_FILE_NAME),
+            usecols=["MAL_ID", "Name"],
+        )
+        anime_titles: dict[int, str] = {
+            int(mal_id): str(name) for mal_id, name in zip(anime_df["MAL_ID"], anime_df["Name"])
+        }
+        hybrid_recommender = HybridRecommender(self.hybrid_config)
+        hybrid_artifact = hybrid_recommender.run(ratings_df, content_artifact, user_artifact, anime_titles)
+
+        return content_artifact, user_artifact, hybrid_artifact
+
+
+if __name__ == "__main__":
+    TrainingPipeline().run()
